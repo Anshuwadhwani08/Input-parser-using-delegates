3fc032c146170ca7128e92597a7e7c6ae0f3827b
Anshu-w <anshu.wadhwani@augmentolabs.com>
2022-11-29 04:23:07 +0530

    initial commit

---
+++ InputParser using delegates/ConsoleReader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputParser_using_delegates
+{
+    public class ConsoleReader
+    {
+        public void Run()
+        {
+            string input;
+            input = Console.ReadLine();
+        }
+        public delegate void onWord();
+        public delegate void onNumber();
+        public delegate void onJunk();
+
+        public void Word()
+        {
+            Console.WriteLine("input string contains group of words");
+            Console.ReadLine();
+        }
+        public void Num()
+        {
+            Console.WriteLine("input string contains group of numbers");
+            Console.ReadLine();
+        }
+        public void Junk()
+        {
+            Console.WriteLine("input string contains special characters");
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            ConsoleReader reader = new ConsoleReader();
+            onWord onword = new onWord(reader.Word);
+            onNumber onnumber = new onNumber(reader.Num);
+            onJunk onjunk = new onJunk(reader.Junk);
+
+            string input;
+            input = Console.ReadLine();
+            if(input.All(char.IsDigit) )
+            {
+                onnumber();
+            }
+            else if (input.All(char.IsLetterOrDigit))
+            {
+                onword();
+            }
+            else if(!(input.All(char.IsLetterOrDigit) )) 
+            {
+                onjunk();
+            }
+
+
+        }
+    }
+}
+
+
---
+++ TestProject1/InputParserTest.cs
@@ -0,0 +1,15 @@
+using InputParser_using_delegates;
+using static InputParser_using_delegates.ConsoleReader;
+
+namespace TestProject1
+{
+    public class InputParserTest
+    {
+        ConsoleReader reader = new ConsoleReader();
+        [Fact]
+        public void only_numbers()
+        {
+            Assert.Equal("input string contains group of numbers",)
+        }
+    }
+}
